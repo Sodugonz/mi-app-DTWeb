ac2d812ef0d6a8e6001065186075be7a6df5d8dc
Gabriel Pais <pais.gabriel@Gmail.com>
2024-01-24 21:08:13 +0100

    Cambio botones por pestañas Cambio ancho de columnas Cambio input types del formulario (eran todos password) Añado ID “divInfo” al bloque que agrupa el formulario de Información general Añadida la clase container-fluid al contenedor principal para que se adapte al ancho completo del navegador. Cambio el valor por defecto de escondido a false para facilitar el trabajo. Lo tendremos que cambiar más adelante He creado el contenedor de la tarjeta Añado variables y posibilidad de cambiar de una pestaña a otra fácilmente con ngIf. Añado también FormsModule Añadido nombre y profesión vinculados a la tarjeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {AsideComponent} from './aside/aside.component';
+import {NavComponent} from './nav/nav.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
