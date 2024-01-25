99ab7ed48c287f4f12079ad716b5d6aad88f327a
Gabriel Pais <pais.gabriel@Gmail.com>
2024-01-25 01:05:10 +0100

    Avances varios
    He reorganizado un poco el HTML, sobre todo en la parte de los paneles de opciones (no cuadraban en el orden interno con cómo aparecen en la página).
    
    He añadido data-binding de interpolación a los estilos de color (he creado tres: primario, secundario y fondo) y a las redes sociales, aunque en este último caso no he hecho que los campos en el formulario metan la información en cada propiedad, queda pendiente.
    
    También he hecho que tu función de escondido afecte a la tarjeta que creé, que antes estaba permanentemente visible.
    
    He incorporado una pequeña tontería y es que al pulsar el botón que pusiste, @sodugonz, haga scroll hacia abajo. Parece que funciona! Así se entiende mejor lo que hace.

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -6,14 +6,34 @@ import {Component} from '@angular/core';
   styleUrl: './aside.component.css'
 })
 export class AsideComponent {
-  escondido : boolean = false;
-  tabActive : number = 1;
+  escondido: boolean = true;
+  tabActive: number = 1;
 
+  persona = {
+    nombre: 'Nombre',
+    profesion: 'Profesión',
+    mail: 'ejemplo@correo.com',
+    movil: '123456789',
+    empresa: 'Empresa',
+    web: 'www.web.com',
+  }
+  social = {
+    facebook: 'facebook',
+    twitter: 'twitter',
+    linkedin: 'linkedin',
+    instagram: 'instagram',
+  }
+  estilo = {
+    color1: '#ffffff',
+    color2: '#f0f0f0',
+    color3: '#212529',
+  }
 
-  nombre : string = 'Nombre';
-  profesion : string = 'Profesión';
-  mail : string = 'ejemplo@correo.com';
-
+  opcionesImagen = [
+    { id: 'imagenRadio1', etiqueta: 'Horizontal' },
+    { id: 'imagenRadio2', etiqueta: 'Vertical' },
+    { id: 'imagenRadio3', etiqueta: 'Cuadrada' }
+  ];
 
 
   constructor() {
@@ -29,25 +49,21 @@ export class AsideComponent {
       case 'estilo':
         console.log('estilo');
         this.tabActive = 1;
-
         break;
 
       case 'opciones':
         console.log('opciones');
         this.tabActive = 2;
-
         break;
 
       case 'info':
         console.log('info');
         this.tabActive = 3;
-
         break;
 
       case 'social':
         console.log('social');
         this.tabActive = 4;
-
         break;
     }
   }
